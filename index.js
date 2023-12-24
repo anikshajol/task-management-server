e43531ac0eb502efac47504b634e8040edb813e5
anikshajol <anikshajol@gmail.com>
2023-12-25 04:19:27 +0600

    all route create

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 // mongodb
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://admin:admin@cluster0.jgojmkc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,15 +33,72 @@ async function run() {
     // todo-list
     app.post("/api/v1/create-todo", async (req, res) => {
       try {
-        const query = req.body;
-        const result = await todoCollection.insertOne(query);
+        const todoData = req.body;
+        // Set the initial status to "todo"
+        todoData.status = "todo";
+
+        const result = await todoCollection.insertOne(todoData);
         res.send(result);
-      } catch (error) {}
+      } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+      }
+    });
+
+    app.put("/api/v1/update-todo-status/:id", async (req, res) => {
+      try {
+        const todoId = req.params.id;
+        const query = { _id: new ObjectId(todoId) };
+        const { status } = req.body;
+        console.log(status);
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: { status },
+        };
+        // Update the status of the todo item
+        const result = await todoCollection.updateOne(
+          query,
+          updateDoc,
+          options
+        );
+
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+
+        res.status(500).send("Internal Server Error");
+      }
     });
 
     app.get("/api/v1/todo-list", async (req, res) => {
       try {
         const result = await todoCollection.find().toArray();
+        res.send(result);
+      } catch (error) {}
+    });
+
+    // todo
+    app.get("/api/v1/todo-list/todo", async (req, res) => {
+      try {
+        const result = await todoCollection.find({ status: "todo" }).toArray();
+        res.send(result);
+      } catch (error) {}
+    });
+    // ongoing
+    app.get("/api/v1/todo-list/ongoing", async (req, res) => {
+      try {
+        const result = await todoCollection
+          .find({ status: "ongoing" })
+          .toArray();
+        res.send(result);
+      } catch (error) {}
+    });
+    // ongoing
+    app.get("/api/v1/todo-list/complete", async (req, res) => {
+      try {
+        const result = await todoCollection
+          .find({ status: "complete" })
+          .toArray();
         res.send(result);
       } catch (error) {}
     });
