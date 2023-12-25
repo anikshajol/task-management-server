26a8bb42a40ec86aad026dbf209e7dc9dff508bf
anikshajol <anikshajol@gmail.com>
2023-12-25 12:17:44 +0600

    finalize

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors());
 // mongodb
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri = `mongodb+srv://admin:admin@cluster0.jgojmkc.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jgojmkc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // connect collection
     const todoCollection = client.db("taskManagement").collection("todoList");
